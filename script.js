970ded67da5a8edd5326cc4b0ccfe8066f26da56
Alexandr Kruzhanovskyi <alexandrkruz76@gmail.com>
2023-04-04 00:03:07 +0300

    add:js

---
+++ script.js
@@ -0,0 +1,6 @@
+saveComments.addEventListener('click', function() {
+    console.log('Name:', userName.value);
+    console.log('Comment:', userComment.value);
+    document.getElementById('userName');
+    document.getElementById('userComment');
+});
