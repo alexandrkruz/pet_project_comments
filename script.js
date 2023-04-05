6667e3feec97c06cd50091ec847daff78eb996bb
AlekseyYefanov <yefanovaleksey@gmail.com>
2023-04-05 10:56:37 +0300

    fix: refacroting

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-saveComments.addEventListener('click', function() {
+const sabeBtn = document.getElementById('saveComments');
+
+sabeBtn.addEventListener('click', saveComment);
+
+const saveComment = function() {
     console.log('Name:', userName.value);
     console.log('Comment:', userComment.value);
     document.getElementById('userName');
     document.getElementById('userComment');
-});
+}
+
+
