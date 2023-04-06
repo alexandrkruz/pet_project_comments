d99b32add7d046f83278affe2b135b3b6ac91e81
Alexandr Kruzhanovskyi <alexandrkruzhanovskyi@MacBook-Pro-Alexandr.local>
2023-04-06 21:07:07 +0300

    fix: clear form

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const sabeBtn = document.getElementById('saveComments');
 
-sabeBtn.addEventListener('click', saveComment);
-
 const saveComment = function() {
     console.log('Name:', userName.value);
     console.log('Comment:', userComment.value);
-    document.getElementById('userName');
-    document.getElementById('userComment');
+    document.getElementById('userName').value = '';
+    document.getElementById('userComment').value = '';
+
 }
+
+sabeBtn.addEventListener('click', saveComment);
 
 
