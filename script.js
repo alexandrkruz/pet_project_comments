c71eb9f6aad1b0b3d075ee0aa1e18fc21bde91ad
AlekseyYefanov <yefanovaleksey@gmail.com>
2023-05-19 17:14:32 +0300

    feat: refactoring

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
 const sabeBtn = document.getElementById('saveComments');
 const commentList = document.getElementById('comment-list');
-const allComments = [{name: 'Alex', comment: 'Some text'}, {name: 'Oleg', comment: 'Some text2'}];
+let allComments = [];
 
-// const saveComment = function() {
+// TODO: add filter by text 
+// 1. Навесить обработчик собития (какой подумать)
+// 2. Получить данные с инпута
+// 3. Сравнить данные с поля с заголовками комментариев array.filter()
 
-//     const userName = document.getElementById('userName');
-//     const userComment = document.getElementById('userComment');
+const searchInput = document.getElementById('search');
 
-//     const newComment = {
-//         name: userName.value,
-//         comment: userComment.value,
-//         date: new Date()
-//     }
+const filteredByText = (event) => {
+  console.log('filteredByText--> ', event.target.value);
+  let filteredComments = [];
+  //array.filter()
+  displayComments(filteredComments)//отфильтрованые коментарии по совпадению;
 
-//     allComments.push(newComment);
+}
 
-//     userName.value = '';
-//     userComment.value = '';
-// }
+
+searchInput.addEventListener('keyup', filteredByText);
+
+
+
+const saveComment = function() {
+
+    const userName = document.getElementById('userName');
+    const userComment = document.getElementById('userComment');
+
+    const newComment = {
+        name: userName.value,
+        comment: userComment.value,
+        date: new Date()
+    }
+
+    allComments.push(newComment);
+
+    userName.value = '';
+    userComment.value = '';
+}
 
 // Кожний коментар записати як окремий блок <article>.....
 // Коли отримав усі коментарі в тегах треба вівести їх на єкран
 // Знайти блок куди віводити і вставии його
 
-// const renderComments = () => {
-//     let comment = '';
+ function displayComments(comments) {
+    let comment = '';
 
-//     for(let i = 0; i < allComments.length; i++) {
-//         comment += `<article>
-//             <h3 class='user-name'>${allComments[i].name}</h3>
-//             <div class='user-comment'>${allComments[i].comment}</div>
-//         </article>`;
-//     }
+    for(let i = 0; i < comments.length; i++) {
+        comment += `<article class='block'>
+            <h3 class='user-name'>${comments[i].title}</h3>
+            <span class='comment-time'>${comments[i].time}</span>
+            <div class='user-comment'>${comments[i].body}</div>
+        </article>`;
+    }
 
-//     commentList.innerHTML = comment;
-// };
-
-// renderComments();
-
+    commentList.innerHTML = comment;
+};
 
 
-// sabeBtn.addEventListener('click', saveComment);
-
-let comments = [];
+sabeBtn.addEventListener('click', saveComment);
 
 async function getUserComments() {
 
     let responce = await fetch('https://run.mocky.io/v3/95fc487f-cc77-4965-824f-c13b582983c0');
     let user = await responce.json();
-    comments = [...user]
+    allComments = [...user]
     displayComments(user);
 }
 
 getUserComments();
-
 
 
 const sortDirectionSelect = document.getElementById("sort-direction");
@@ -74,19 +89,18 @@ function sortedComments() {
   displayComments(comments);
 }
 
-  function displayComments(comments) {
+//   function displayComments(comments) {
  
-    
-    const container = document.getElementById("comment-list");
-    container.innerHTML = "";
-    comments.forEach((comment) => {
-    const div = document.createElement("div");
-    div.textContent = comment.title;
-    container.appendChild(div);
-  });
+//     const container = document.getElementById("comment-list");
+//     container.innerHTML = "";
+//     comments.forEach((comment) => {
+//     const div = document.createElement("div");
+//     div.textContent = comment.title;
+//     container.appendChild(div);
+//   });
 
 
-}
+// }
 
 
 
